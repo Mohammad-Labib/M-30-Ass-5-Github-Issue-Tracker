1006496d4aefeb357736d0b77ec1081e7f5f3efe
Mohammad-Labib <friendlabib@gmail.com>
2026-03-09 00:45:06 +0600

    card add

--- script/home.js
+++ script/home.js
@@ -1,31 +1,5 @@
-// const loadingPart = () => {
-//     fetch("https://phi-lab-server.vercel.app/api/v1/lab/partOns")
-//     .then((res) => res.json())
-//     .then((json) => displayPart(json.data))
-// };
-
-// const displayPart = (part) =>{
-//     // console.log(part);
-
-//     const levelContainer = document.getElementById("level-container");
-//     levelContainer.innerHTML = "";
-//     for(let partOn of part){
-//         // console.log(partOn);
-//         const thisDiv = document.createElement("div");
-//         thisDiv.innerHTML = `
-//                   <button class="btn btn-outline btn-primary w-[120px] h-[40px] text-[20px]">All - ${partOn.id}</button>
-//         `;
-
-//         levelContainer.append(thisDiv)
-//     }
-// }
-// loadingPart()
-
-
 
 let thisData = [];
-
-
 const loadingPart = () => {
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((res) => res.json())
@@ -34,6 +8,21 @@ const loadingPart = () => {
         displayCards(thisData); 
     })
 };
+
+const datailsPart = async(id) =>{
+    const url = (`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    console.log(url);
+    const res = await fetch(url)
+    const datails = await res.json();
+    displayInfo(datails.data)
+}
+
+const displayInfo = (parts) => {
+    console.log(parts)
+    const datailsBox = document.getElementById("datails-container")
+    // datailsBox.innerHTML="Hi i am labib"
+    document.getElementById("parts_modal").showModal();
+}
 
 
 const displayCards = (part) => {
@@ -46,42 +35,78 @@ const displayCards = (part) => {
         const cardDiv = document.createElement("div");
         cardDiv.className = `card bg-white shadow rounded p-4 space-y-2 border-t-4 ${borderColor}`;
         cardDiv.innerHTML = `
+           <div onclick="datailsPart(${partOn.id}) " class="space-y-4">
             <div class="flex justify-between items-center ">
                 <img src="../assets/Open-Status.png" alt="">
-                <h1 class="bg-[#FEECEC] rounded font-semibold px-2 text-center text-sm">${partOn.priority}</h1>
+                <h1 class="bg-[#FEECEC] rounded font-semibold px-2 text-center text-[12px]">${partOn.priority}</h1>
             </div>
             <div>
-                <h1 class="font-semibold text-[20px]">${partOn.title}</h1>
-                <p class="text-[#64748B] text-sm">${partOn.description || 'No description available'}</p>
+                <h1 class="font-semibold text-[22px]">${partOn.title}</h1>
+                <p class="text-[#64748B] text-sm">${partOn.description || 'No data found'}</p>
             </div>
             <div class="flex gap-2">
-                 <h1 class="text-[#EF4444] bg-[#FEECEC] rounded font-semibold px-2 text-xs text-center">Bug</h1>
-                 <h1 class="text-[#D97706] bg-[#FEECEC] rounded font-semibold px-2 text-xs text-center">help wanted</h1>
+                 <h1 class="text-[#EF4444] bg-[#FEECEC] rounded font-semibold px-2 text-[16px] text-center">Bug</h1>
+                 <h1 class="text-[#D97706] bg-[#FEECEC] rounded font-semibold px-2 text-[16px] text-center">help wanted</h1>
             </div>
-            <hr>
+            <hr class="text-[#64748B] >
             <div>
                 <h1 class="text-[#64748B] text-xs">#${partOn.id} by ${partOn.author}</h1>
-                <p class="text-[#64748B] text-xs">${partOn.updatedAt}</p>
+                <p class="text-[#64748B] text-xs">${partOn.createdAt}</p>
             </div>
+           </div>
         `;
         cardContainer.append(cardDiv);
     });
 }
 
-// ৩. Button filtering logic
+
 document.getElementById('allBtn').addEventListener('click', () => {
-    displayCards(thisData); // Shob dekhabe
+    displayCards(thisData); 
 });
 
 document.getElementById('openBtn').addEventListener('click', () => {
     const openData = thisData.filter(item => item.status === 'open');
-    displayCards(openData); // Shudhu open gulo
+    displayCards(openData); 
 });
 
 document.getElementById('close-Btn').addEventListener('click', () => {
     const closedData = thisData.filter(item => item.status === 'closed');
-    displayCards(closedData); // Shudhu closed gulo
+    displayCards(closedData); 
 });
 
-// App start hole function call kora
+
 loadingPart();
+
+//remove and active btn
+function removeActive(){
+    const buttons = document.querySelectorAll(".filter-btn");
+
+    for(let btn of buttons){
+        btn.classList.remove("bg-blue-500");
+        btn.classList.remove("text-white");
+    }
+};
+
+document.getElementById('allBtn').addEventListener('click', function () {
+    removeActive();
+    this.classList.add("bg-blue-500","text-white");
+
+    displayCards(thisData); 
+});
+
+document.getElementById('openBtn').addEventListener('click', function () {
+    removeActive();
+    this.classList.add("bg-blue-500","text-white");
+
+    const openData = thisData.filter(item => item.status === 'open');
+    displayCards(openData); 
+});
+
+document.getElementById('close-Btn').addEventListener('click', function () {
+    removeActive();
+    this.classList.add("bg-blue-500","text-white");
+
+    const closedData = thisData.filter(item => item.status === 'closed');
+    displayCards(closedData); 
+});
+
